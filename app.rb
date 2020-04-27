a7c6cbc5e3edf9061dcee0bfd1a6785142c2dd30
pauladarias <pauladarias@gmail.com>
2020-04-27 18:11:04 +0100

    Intro to params

--- app.rb
+++ app.rb
@@ -14,7 +14,13 @@ get '/add' do
     "add a string"
 end
 
-get '/cat' do 
+get '/random-cat' do 
     @random_name = ["Andrew", "Paula", "Marius"].sample
+    erb(:index)
+end 
+
+get '/named-cat' do 
+    p params
+    @random_name = params[:makers]
     erb(:index)
 end 
